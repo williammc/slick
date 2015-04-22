c2d90b9e08fb8521529725956eff1b0a4386a2cd
William McCohen <william.mccohen@gmail.com>
2015-04-22 15:59:50 +0200

    fix commit

--- slick/test/test_scene_triangulation.cc
+++ slick/test/test_scene_triangulation.cc
@@ -4,49 +4,51 @@
 
 #include "slick/math/se3.h"
 
-using namespace Eigen;
-using namespace slick;
-
 int test_triangulate() {
   std::cout << "generting 3D points..." << std::endl;
-  std::vector<Matrix<SlickScalar,3,1> > v3_points(100);
+  std::vector<Eigen::Matrix<slick::SlickScalar, 3, 1>> v3_points(100);
   for (size_t i = 0; i < v3_points.size(); ++i) {
     v3_points[i].setRandom();
-    v3_points[i] *= 20.0;  // scale up to value range to [-100,100]
-    if(v3_points[i][2] < 0)
+    v3_points[i] *= 20.0; // scale up to value range to [-100,100]
+    if (v3_points[i][2] < 0)
       v3_points[i][2] = -v3_points[i][2];
     v3_points[i][2] = 17.0; // all 3D points are on the same plane
   }
   // generates a  3degree rotation around axis-y & 1unit translate along axis-a
-  SE3 pose1;
-  Matrix<SlickScalar,3,1> v3(0,3.0*M_PI/180.0,0);
-  SO3Group<SlickScalar> so3_r(v3);
-  SE3Group<SlickScalar> pose2(so3_r,Matrix<SlickScalar,3,1>(-1.0,0.0,0.0));
+  slick::SE3 pose1;
+  Eigen::Matrix<slick::SlickScalar, 3, 1> v3(0, 3.0 * M_PI / 180.0, 0);
+  slick::SO3Group<slick::SlickScalar> so3_r(v3);
+  slick::SE3Group<slick::SlickScalar> pose2(
+      so3_r, Eigen::Matrix<slick::SlickScalar, 3, 1>(-1.0, 0.0, 0.0));
 
-  std::vector<SlickScalar> norms;
-  for (int i=0; i<v3_points.size(); ++i) {
-    Matrix<SlickScalar,2,1> v2_p1 = project(pose1*v3_points[i]);
-    Matrix<SlickScalar,2,1> v2_p2 = project(pose2*v3_points[i]);
-    std::pair<Eigen::Matrix<SlickScalar,3,1>,SlickScalar> res = triangulate(v2_p1, v2_p2, pose1, pose2);
+  std::vector<slick::SlickScalar> norms;
+  for (int i = 0; i < v3_points.size(); ++i) {
+    Eigen::Matrix<slick::SlickScalar, 2, 1> v2_p1 = slick::project(pose1 * v3_points[i]);
+    Eigen::Matrix<slick::SlickScalar, 2, 1> v2_p2 = slick::project(pose2 * v3_points[i]);
+    std::pair<Eigen::Matrix<slick::SlickScalar, 3, 1>, slick::SlickScalar> res =
+        slick::triangulate(v2_p1, v2_p2, pose1, pose2);
 
-    std::cout << "norm(real-estimate):" << (v3_points[i]-res.first).norm() << std::endl;
-    norms.push_back((v3_points[i]-res.first).norm());
+    std::cout << "norm(real-estimate):" << (v3_points[i] - res.first).norm()
+              << std::endl;
+    norms.push_back((v3_points[i] - res.first).norm());
   }
 
-  SlickScalar sum_norm(0.0);
-  for (std::vector<SlickScalar>::iterator j=norms.begin();j!=norms.end();++j)
-      sum_norm += *j;
-  std::cout << "sum_norm:" << "mean_norm:" << sum_norm/norms.size() << std::endl;
-  if(sum_norm/norms.size() >1.e-9)
+  slick::SlickScalar sum_norm(0.0);
+  for (std::vector<slick::SlickScalar>::iterator j = norms.begin();
+       j != norms.end(); ++j)
+    sum_norm += *j;
+  std::cout << "sum_norm:"
+            << "mean_norm:" << sum_norm / norms.size() << std::endl;
+  if (sum_norm / norms.size() > 1.e-9)
     return -1;
   return 1;
 }
 
-int main (int argc, char** argv) {
+int main(int argc, char **argv) {
   bool ok = true, final_ok = true;
   ok = test_triangulate();
   std::cout << "test_triangulate():" << ok << std::endl;
   final_ok &= ok;
 
-  return (final_ok) ? 1 : -1;
+  return (final_ok) ? 0 : -1;
 }
--- slick/test/test_util_handeye.cc
+++ slick/test/test_util_handeye.cc
@@ -3,37 +3,36 @@
 #include <tuple>
 #include "slick/util/handeye.h"
 
-using namespace slick;
-using namespace std;
-
 int test_computeHandEye() {
-  stringstream ss;
-  ss << Slick_ROOT << "/data/" << "handeye-1.txt";
-  fstream fs(ss.str().c_str(), std::ios_base::in);
-  //vector<SO3> sensorworld2sensor_rots, cam2vision_rots;
-  Eigen::Matrix<SlickScalar, 3, 1> temp_v;
-  for (int i=0; i<6; ++i) {
+  std::stringstream ss;
+  ss << Slick_ROOT << "/data/"
+     << "handeye-1.txt";
+  std::fstream fs(ss.str().c_str(), std::ios_base::in);
+  // vector<SO3> sensorworld2sensor_rots, cam2vision_rots;
+  Eigen::Matrix<slick::SlickScalar, 3, 1> temp_v;
+  for (int i = 0; i < 6; ++i) {
     fs >> temp_v[0] >> temp_v[1] >> temp_v[2];
-    //SO3 sw2s_rot(temp_v);
+    // SO3 sw2s_rot(temp_v);
 
     fs >> temp_v[0] >> temp_v[1] >> temp_v[2];
-    //SO3 c2v_rot(temp_v);
-    //sensorworld2sensor_rots.push_back(sw2s_rot);
-    //cam2vision_rots.push_back(c2v_rot);
+    // SO3 c2v_rot(temp_v);
+    // sensorworld2sensor_rots.push_back(sw2s_rot);
+    // cam2vision_rots.push_back(c2v_rot);
   }
 
-  //std::pair<SO3 , SO3 > res  = ComputeHandEye( sensorworld2sensor_rots, cam2vision_rots );
+  // std::pair<SO3 , SO3 > res  = ComputeHandEye( sensorworld2sensor_rots,
+  // cam2vision_rots );
 
-  //cout << "sensor2cam:\n" << res.first << endl;
-  //cout << "vision2sensorworld:\n" << res.second << endl;
+  // cout << "sensor2cam:\n" << res.first << endl;
+  // cout << "vision2sensorworld:\n" << res.second << endl;
 
   return 1;
 }
 
-int main (int argc, char** argv) {
-  bool final_ok=true;
+int main(int argc, char **argv) {
+  bool final_ok = true;
   bool ok = test_computeHandEye();
-  cout << "test_computeHandEye():" << ok  << endl;
+  printf("test_computeHandEye():%u\n", ok);
   final_ok &= ok;
-  return (final_ok)?1:-1;
+  return (final_ok) ? 0 : -1;
 }
